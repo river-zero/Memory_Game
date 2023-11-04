ff7077c4ddf0b4786bb46fd9e4a1994009b30231
river-zero <lero_ro@naver.com>
2023-11-04 19:40:06 +0900

    추가: 빈 윈도우 세팅

---
+++ README.md
@@ -0,0 +1,33 @@
+## 짝 맞추기 게임
+뒤집힌 카드 더미에서 한 장씩 앞면을 확인할 수 있게 뒤집습니다. 짝이 맞으면 없어지고 모든 카드의 짝을 맞추게 되면 클리어하는 방식의 게임입니다.
+
+## 플레이 영상
+![Memory_Game_GIF](memory.gif)
+
+## 기능
+
+
+
+ 
+<!-- ## 기획
+- 카드에는 곰, 늑대, 용 세 가지 종류가 있습니다.
+- 보드판에 8 x 5만큼의 카드들이 무작위로 표시됩니다.
+- 한 장씩 마우스를 클릭하면 카드가 뒤집힙니다.
+- 선택한 두 개의 카드가 짝이 맞으면 해당 카드는 보드에서 삭제됩니다.
+- 짝이 맞지 않는 경우라면 두 개의 카드 모두 다시 뒷면으로 뒤집습니다.
+- 카드를 뒤집을 때마다 우측에 클릭수가 증가합니다. -->
+
+## 리소스 목록
+|   이름    |    설명     |
+| :-------: | :---------: |
+| Board.jpg |    배경     |
+| Back.png  |  카드 뒷면  |
+|   7.png   |   7 카드    |
+|  Ace.png  | 에이스 카드 |
+| Joker.png |  조커 카드  |
+| King.png  |   킹 카드   |
+
+
+## 출처
+- https://opengameart.org/content/vintage-playing-cards?page=1
+- https://opengameart.org/content/weathered-wood-seamless-texture-with-normalmap
---
+++ WinMain.cpp
@@ -0,0 +1,99 @@
+#include <windows.h>
+#include <gdiplus.h>
+
+#pragma comment (lib, "Gdiplus.lib")
+
+using namespace Gdiplus;
+
+const wchar_t gClassName[] = L"WindowClass";
+
+LRESULT CALLBACK WindowProc(
+	HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam
+);
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd) {
+	GdiplusStartupInput gsi;
+	ULONG_PTR token;
+	GdiplusStartup(&token, &gsi, nullptr);
+
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpszClassName = gClassName;
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WindowProc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	if (!RegisterClassEx(&wc)) {
+		MessageBox(nullptr, L"Failed to Register", L"Error", MB_OK);
+		return 0;
+	}
+
+	RECT wr = { 0, 0, 1024, 768 };
+	AdjustWindowRect(&wr, WS_OVERLAPPED | WS_SYSMENU, FALSE);
+
+	HWND hwnd = CreateWindowEx(
+		0,
+		gClassName,
+		L"Memory Game",
+		WS_OVERLAPPED | WS_SYSMENU,
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	if (hwnd == nullptr) {
+		MessageBox(nullptr, L"Failed to Create", L"Error", MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nShowCmd);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	GdiplusShutdown(token);
+	return static_cast<int>(msg.wParam);
+}
+
+void OnPaint(HWND hwnd) {
+	PAINTSTRUCT ps;
+	HDC hdc = BeginPaint(hwnd, &ps);
+	Graphics graphics(hdc);
+
+	EndPaint(hwnd, &ps);
+}
+
+LRESULT WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+	case WM_LBUTTONUP:
+		break;
+	case WM_PAINT:
+		OnPaint(hwnd);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
